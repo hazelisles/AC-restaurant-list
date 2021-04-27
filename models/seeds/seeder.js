bf1d1fb338ee645f011ceb5919c5a70effb2d7de
hazelisles <zhccj19@gmail.com>
2021-04-27 14:41:15 +0800

    feat: add restaurant seeder

---
+++ models/seeds/seeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const R = require('../restaurant')
+const { results: restaurants } = require('C:\\Users\\zhccj\\AlphaCamp\\2-3\\restaurant_list\\restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurant-list',  { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < restaurants.length; i++) {
+    R.create(restaurants[i])
+  }
+})
